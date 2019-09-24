0477a6519231d4e2567b55148bd4ecdbd5fae4ce
sawdsf <p.lev4ik@yandex.ru>
2019-09-24 19:14:28 +0300

    bsearch

---
+++ 1st_task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1st_task
+{
+    class _1st_task
+    {
+        public int Bsearch(int[] Mass, int x)
+        {
+            int i = 0;
+            while (Mass[i] >= x)
+            { i++; }
+            return i;
+        }
+    }
+}
