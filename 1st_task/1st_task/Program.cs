bbaa050b217b4c2952f95078fbd167c8ad4440e9
sawdsf <p.lev4ik@yandex.ru>
2019-09-24 19:15:37 +0300

    bsearch

---
+++ 1st_task/1st_task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1st_task
+{
+    class Program
+    {
+        static int[] M = { 15, 14, 13, 12, 11, 10 };
+        static void Main(string[] args)
+        {
+            _1st_task a = new _1st_task();
+            Console.WriteLine(a.Bsearch(M,13));
+        }
+    }
+}
